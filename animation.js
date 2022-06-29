715fa7cc2ce8a9f512d30bad8e070faca003c703
Jundy <junayd.guelmani18@gmail.com>
2022-06-29 12:04:41 +0200

    MAJ

--- animation.js
+++ animation.js
@@ -23,6 +23,116 @@ function maFonction3() {
   element.scrollIntoView();
 }
 
+//Image aleatoire
+/*
+var pos_x = Math.round(Math.random()*4000);
+
+
+var
+pos_y = Math.round(Math.random()*900);
+var Obj = document.getElementById("image1")
+
+
+Obj.style.left = pos_x +"px";
+
+Obj.style.top  = pos_y +"px";
+
+
+//-- Suivante
+
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+Obj = document.getElementById("image2")
+
+Obj.style.left = pos_x +"px";
+
+Obj.style.top  = pos_y +"px";
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj2 = document.getElementById("image3")
+
+Obj2.style.left = pos_x +"px";
+
+Obj2.style.top  = pos_y +"px";
+console.log(Obj)
+console.log(pos_x , pos_y)
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3050);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj3 = document.getElementById("image4")
+
+Obj3.style.left = pos_x +"px";
+
+Obj3.style.top  = pos_y +"px";
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj4 = document.getElementById("image5")
+
+Obj4.style.left = pos_x +"px";
+
+Obj4.style.top  = pos_y +"px";
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj5 = document.getElementById("image6")
+
+Obj5.style.left = pos_x +"px";
+
+Obj5.style.top  = pos_y +"px";
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj6 = document.getElementById("image7")
+
+Obj6.style.left = pos_x +"px";
+
+Obj6.style.top  = pos_y +"px";
+
+//Suivant
+
+pos_x = Math.round(Math.random()*3000);
+
+pos_y = Math.round(Math.random()*900);
+
+var Obj7 = document.getElementById("image8")
+
+Obj7.style.left = pos_x +"px";
+
+Obj7.style.top  = pos_y +"px";
+*/
+
+
+
+
+
+
+
+
 
 
 /*var i = 0;
