7a0f4d1178bc27ca4ca17a7a03718492b51aca59
Jundy <junayd.guelmani18@gmail.com>
2022-06-29 14:12:19 +0200

    MAJ

--- animation.js
+++ animation.js
@@ -23,6 +23,51 @@ function maFonction3() {
   element.scrollIntoView();
 }
 
+// Animation texte
+const animatedTexts = document.querySelectorAll('.animated-text');
+
+
+function animateText(animatedText) {
+  const text = animatedText.textContent;
+  let speed = animatedText.getAttribute("speed");
+  let delay = animatedText.getAttribute("delay");
+  if (speed == null) {
+    speed = 150;
+  }
+  if (delay == null) {
+    delay = 0;
+  }
+  animatedText.innerHTML = '';
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == ' ') {
+      animatedText.innerHTML += ' ';
+    }
+    animatedText.innerHTML += `<span>${text[i]}</span>`;
+  }
+
+  setTimeout(function () {
+    let idx = 0;
+    function writeChar() {
+      const span = animatedText.querySelectorAll('span')[idx];
+      span.classList.add('fade');
+      idx++;
+      if (idx == text.length) {
+        clearInterval(writeCharInterval);
+      }
+    }
+  
+    let writeCharInterval = setInterval(writeChar, speed);
+  }, delay);
+}
+
+animatedTexts.forEach(animateText);
+
+
+
+
+
+
+
 //Image aleatoire
 /*
 var pos_x = Math.round(Math.random()*4000);
